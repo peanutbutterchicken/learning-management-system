a35f2b74e0cf93286bd76e8367879ee99715f161
peanutbutterchicken <markjosephvictoriano20@gmail.com>
2025-06-03 13:25:15 +0800

    Added: ConfirmationEmail JFrame

--- login-signup/src/view/App.java
+++ login-signup/src/view/App.java
@@ -1,8 +1,22 @@
 package view;
-
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        Signin_view signin_view = new Signin_view();
-        signin_view.setVisible(true);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Action: ");
+        int action = sc.nextInt();
+
+        do{
+            switch(action){
+                case 1:
+                ConfirmEmail confirmEmail = new ConfirmEmail();
+                confirmEmail.setVisible(true);
+                break;
+                default:
+                break;
+            }
+        }
+        while(action!=1);
+        
     }
 }
---
+++ login-signup/src/view/ConfirmEmail.java
@@ -0,0 +1,85 @@
+package view;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.util.function.ToDoubleBiFunction;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import model.DesignsAndFormat;
+import model.LoadAndResizeImage;
+
+public class ConfirmEmail extends JFrame {
+    
+    LoadAndResizeImage loadAndResizeImage = new LoadAndResizeImage();
+    DesignsAndFormat designsAndFormat = new DesignsAndFormat();
+    ConfirmEmail(){
+        initComponents();
+    }
+
+    private void initComponents(){
+        ImageIcon confirmEmailImage = loadAndResizeImage.loadAndResize("/resources/images/email-verification.png", 180, 180);
+        JLabel lbConfirmEmailImageContainer = new JLabel(confirmEmailImage);
+
+        JPanel horizontalPanel1 = new JPanel();
+        horizontalPanel1.setLayout(new BoxLayout(horizontalPanel1, BoxLayout.X_AXIS));
+        horizontalPanel1.setBackground(designsAndFormat.mainBackgroundColor());
+        horizontalPanel1.setBorder(BorderFactory.createEmptyBorder(30, 0, 0, 0));
+        horizontalPanel1.setAlignmentX(CENTER_ALIGNMENT);
+        horizontalPanel1.add(lbConfirmEmailImageContainer);
+
+
+        JLabel lbConfirmationEmailTextHeader = new JLabel("Email Confirmation");
+        lbConfirmationEmailTextHeader.setFont(designsAndFormat.mainFont().deriveFont(24f));
+        lbConfirmationEmailTextHeader.setAlignmentX(CENTER_ALIGNMENT);
+
+        JPanel horizontalPanel2 = new JPanel();
+        horizontalPanel2.setLayout(new BoxLayout(horizontalPanel2, BoxLayout.X_AXIS));
+        horizontalPanel2.setBackground(designsAndFormat.mainBackgroundColor());
+        horizontalPanel2.setAlignmentX(CENTER_ALIGNMENT);
+        horizontalPanel2.add(lbConfirmationEmailTextHeader);
+
+
+        JLabel lbConfirmationEmailText1 = new JLabel("We have sent verification email to placeholder@gmail.com to confirm");
+        lbConfirmationEmailText1.setFont(designsAndFormat.mainFont().deriveFont(12f));
+        lbConfirmationEmailText1.setAlignmentX(CENTER_ALIGNMENT);
+        
+        JLabel lbConfirmationEmailText2 = new JLabel("the validity of your email address. After receiving the email");
+        lbConfirmationEmailText2.setFont(designsAndFormat.mainFont().deriveFont(12f));
+        lbConfirmationEmailText2.setAlignmentX(CENTER_ALIGNMENT);
+
+        JLabel lbConfirmationEmailText3 = new JLabel("follow the link provided to complete your registration.");
+        lbConfirmationEmailText3.setFont(designsAndFormat.mainFont().deriveFont(12f));
+        lbConfirmationEmailText3.setAlignmentX(CENTER_ALIGNMENT);
+
+        JPanel horizontalPanel3 = new JPanel();
+        horizontalPanel3.setLayout(new BoxLayout(horizontalPanel3, BoxLayout.Y_AXIS));
+        horizontalPanel3.setAlignmentX(CENTER_ALIGNMENT);
+        horizontalPanel3.setBackground(designsAndFormat.mainBackgroundColor());
+        horizontalPanel3.add(lbConfirmationEmailText1);
+        horizontalPanel3.add(lbConfirmationEmailText2);
+        horizontalPanel3.add(lbConfirmationEmailText3);
+
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.setBackground(designsAndFormat.mainBackgroundColor());
+        mainPanel.add(horizontalPanel1);
+        mainPanel.add(horizontalPanel2);
+        mainPanel.add(horizontalPanel3);
+
+        add(mainPanel);
+        setSize(650, 450);
+        setTitle("Login");
+        setSize(650,450);
+        setMinimumSize(new Dimension(300, 300));
+        setDefaultCloseOperation(Signin_view.EXIT_ON_CLOSE);
+        setVisible(true);
+        setLocationRelativeTo(null);
+    }
+}
