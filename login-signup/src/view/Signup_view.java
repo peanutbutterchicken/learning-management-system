ec3bac7b0adc70a9ea80063843981ccc76c6a1e0
peanutbutterchicken <markjosephvictoriano20@gmail.com>
2025-06-03 20:44:49 +0800

    Removed unused import statement for Array in Signup_view.java

--- login-signup/src/view/Signup_view.java
+++ login-signup/src/view/Signup_view.java
@@ -12,7 +12,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 import javax.swing.BorderFactory;
